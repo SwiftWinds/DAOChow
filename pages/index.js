9e9c3957230c2875e80d840f080b0162af4013be
SwiftWinds <12981958+SwiftWinds@users.noreply.github.com>
2022-09-10 21:24:29 -0700

    Make it centered at US

--- pages/index.js
+++ pages/index.js
@@ -1,127 +1,132 @@
 import Head from "next/head";
 
-import GoogleMapReact from 'google-map-react';
+import GoogleMapReact from "google-map-react";
 
 const Marker = () => {
-  return <>
-    <div onClick={() => console.log("clicked")} className="pin cursor-pointer"></div>
-    <div className="pulse"></div>
-  </>
-}
+  return (
+    <>
+      <div
+        onClick={() => console.log("clicked")}
+        className="pin cursor-pointer"
+      ></div>
+      <div className="pulse"></div>
+    </>
+  );
+};
 
 const locations = {
-  "features": [
+  features: [
     {
-      "type": "Feature",
-      "properties": {
-        "title": "Isla Vista, CA"
+      type: "Feature",
+      properties: {
+        title: "Isla Vista, CA",
       },
-      "geometry": {
-        "coordinates": [-119.85897295511914, 34.415143997683394],
-        "type": "Point"
-      }
+      geometry: {
+        coordinates: [-119.85897295511914, 34.415143997683394],
+        type: "Point",
+      },
     },
     {
-      "type": "Feature",
-      "properties": {
-        "title": "Oakland, CA"
+      type: "Feature",
+      properties: {
+        title: "Oakland, CA",
       },
-      "geometry": {
-        "coordinates": [-122.27244577304657, 37.8049573528661],
-        "type": "Point"
-      }
+      geometry: {
+        coordinates: [-122.27244577304657, 37.8049573528661],
+        type: "Point",
+      },
     },
     {
-      "type": "Feature",
-      "properties": {
-        "title": "Los Angeles, CA"
+      type: "Feature",
+      properties: {
+        title: "Los Angeles, CA",
       },
-      "geometry": {
-        "coordinates": [-118.24354695451103, 34.05244298548162],
-        "type": "Point"
-      }
+      geometry: {
+        coordinates: [-118.24354695451103, 34.05244298548162],
+        type: "Point",
+      },
     },
     {
-      "type": "Feature",
-      "properties": {
-        "title": "Lower Manhattan, NY"
+      type: "Feature",
+      properties: {
+        title: "Lower Manhattan, NY",
       },
-      "geometry": {
-        "coordinates": [-73.9818055594482, 40.72646200200845],
-        "type": "Point"
-      }
+      geometry: {
+        coordinates: [-73.9818055594482, 40.72646200200845],
+        type: "Point",
+      },
     },
     {
-      "type": "Feature",
-      "properties": {
-        "title": "Miami, FL"
+      type: "Feature",
+      properties: {
+        title: "Miami, FL",
       },
-      "geometry": {
-        "coordinates": [-80.19604743738999, 25.775976998165707],
-        "type": "Point"
-      }
+      geometry: {
+        coordinates: [-80.19604743738999, 25.775976998165707],
+        type: "Point",
+      },
     },
     {
-      "type": "Feature",
-      "properties": {
-        "title": "Dallas City, TX"
+      type: "Feature",
+      properties: {
+        title: "Dallas City, TX",
       },
-      "geometry": {
-        "coordinates": [-96.79743906160773, 32.77744363711109],
-        "type": "Point"
-      }
+      geometry: {
+        coordinates: [-96.79743906160773, 32.77744363711109],
+        type: "Point",
+      },
     },
     {
-      "type": "Feature",
-      "properties": {
-        "title": "Denver, CO"
+      type: "Feature",
+      properties: {
+        title: "Denver, CO",
       },
-      "geometry": {
-        "coordinates": [-104.98752648208819, 39.738544116121176],
-        "type": "Point"
-      }
+      geometry: {
+        coordinates: [-104.98752648208819, 39.738544116121176],
+        type: "Point",
+      },
     },
     {
-      "type": "Feature",
-      "properties": {
-        "title": "Chicago, IL"
+      type: "Feature",
+      properties: {
+        title: "Chicago, IL",
       },
-      "geometry": {
-        "coordinates": [-87.69718852518804, 41.9033605869435],
-        "type": "Point"
-      }
+      geometry: {
+        coordinates: [-87.69718852518804, 41.9033605869435],
+        type: "Point",
+      },
     },
     {
-      "type": "Feature",
-      "properties": {
-        "title": "Nashville, TN"
+      type: "Feature",
+      properties: {
+        title: "Nashville, TN",
       },
-      "geometry": {
-        "coordinates": [-86.78166875632597, 36.162839200942756],
-        "type": "Point"
-      }
+      geometry: {
+        coordinates: [-86.78166875632597, 36.162839200942756],
+        type: "Point",
+      },
     },
     {
-      "type": "Feature",
-      "properties": {
-        "title": "Raleigh, NC"
+      type: "Feature",
+      properties: {
+        title: "Raleigh, NC",
       },
-      "geometry": {
-        "coordinates": [-78.63632373575874, 35.778131223351224],
-        "type": "Point"
-      }
-    }
+      geometry: {
+        coordinates: [-78.63632373575874, 35.778131223351224],
+        type: "Point",
+      },
+    },
   ],
-  "type": "FeatureCollection"
+  type: "FeatureCollection",
 };
 
 export default function Home() {
   const defaultProps = {
     center: {
-      lat: 59.955413, 
-      lng: 30.337844
+      lat: 40.517994,
+      lng: -100.711058,
     },
-    zoom: 15
+    zoom: 4,
   };
 
   return (
@@ -142,7 +147,9 @@ export default function Home() {
             <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
               <div style={{ height: "100vh", width: "100%" }}>
                 <GoogleMapReact
-                  bootstrapURLKeys={{ key: "AIzaSyDEvHH5eEFNAFSSN8mdlbk7Qs5Pk58o1T4" }}
+                  bootstrapURLKeys={{
+                    key: "AIzaSyDEvHH5eEFNAFSSN8mdlbk7Qs5Pk58o1T4",
+                  }}
                   defaultCenter={defaultProps.center}
                   defaultZoom={defaultProps.zoom}
                 >
@@ -154,7 +161,8 @@ export default function Home() {
                       onClick={() => {
                         setSelectedLocation(location);
                       }}
-                  />))}
+                    />
+                  ))}
                 </GoogleMapReact>
               </div>
             </div>
