b140d9c6e0a5a3d499f4b606edf797c114563664
unknown <calebee0462@gmail.com>
2022-09-11 01:40:44 -0700

    test

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import "../styles/globals.css";
 
-import { Fragment } from "react";
+import { Fragment, useState } from 'react'
+import { CheckIcon } from '@heroicons/react/24/outline'
 import { useRouter } from "next/router";
-import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Dialog, Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const user = {
@@ -34,6 +35,8 @@ function MyApp({ Component, pageProps }) {
     ...route,
     current: route.href === path,
   }));
+  const [open, setOpen] = useState(false)
+
   return (
     <>
       <Disclosure as="nav" className="border-b border-gray-200 bg-white">
@@ -67,11 +70,11 @@ function MyApp({ Component, pageProps }) {
                 </div>
                 <div className="hidden sm:ml-6 sm:flex sm:items-center">
                   <button
+                    onClick={() => setOpen(true)}
                     type="button"
-                    className="rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    className="inline-flex items-center rounded-md border border-transparent mr-2 bg-indigo-600 px-5 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                   >
-                    <span className="sr-only">View notifications</span>
-                    <BellIcon className="h-6 w-6" aria-hidden="true" />
+                    Contribute
                   </button>
 
                   {/* Profile dropdown */}
@@ -113,6 +116,7 @@ function MyApp({ Component, pageProps }) {
                         ))}
                       </Menu.Items>
                     </Transition>
+
                   </Menu>
                 </div>
                 <div className="-mr-2 flex items-center sm:hidden">
@@ -190,6 +194,62 @@ function MyApp({ Component, pageProps }) {
           </>
         )}
       </Disclosure>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={setOpen}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 z-10 overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+                enterTo="opacity-100 translate-y-0 sm:scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+                leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              >
+                <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-sm sm:p-6">
+                  <div>
+                    <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full bg-green-100">
+                      <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
+                    </div>
+                    <div className="mt-3 text-center sm:mt-5">
+                      <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
+                        Payment successful
+                      </Dialog.Title>
+                      <div className="mt-2">
+                        <p className="text-sm text-gray-500">
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur amet labore.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="mt-5 sm:mt-6">
+                    <button
+                      type="button"
+                      className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm"
+                      onClick={() => setOpen(false)}
+                    >
+                      Go back to dashboard
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
       <Component {...pageProps} />
     </>
   );
